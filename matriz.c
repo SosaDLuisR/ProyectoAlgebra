bee22631b5d995b17b38bc0c2c29e1e473b153ec
Luis Rubén Sosa <99470070+SosaDLuisR@users.noreply.github.com>
2022-04-29 19:09:06 -0500

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -3,25 +3,23 @@ int main()
 {
 	int f,c,i,j,A[10][10];
 	printf("\n\nSolucion de una matriz por metodo de Gauss-Jordan");
+	printf("\Damos por hecho que sabes que es una matriz aumentada");
+	printf("\Ahora te solicitaremos los datos para que los ingreses, gracias :)");
 	printf("\n\nCuantas filas tendra la matriz? ");
 	scanf("%i",&f);
 	printf("\nCuantas columnas tendra la matriz? ");
 	scanf("%i",&c);
 	printf("\n\nIntroduce los elementos que conforman la matriz\n");
-	for(i=1;i<=c;i=i+1)
-	{
-		for(j=1;j<=f;j=j+1)
-		{
+	for(i=1;i<=c;i=i+1) {
+		for(j=1;j<=f;j=j+1) {
 			printf("\nDame un elemento para A[%i][%i]: \t",i,j);
 			scanf("%i",&A[i][j]);
 		}
 	}
 	//*fin captura de matriz*//
 	printf("\nLa matriz que ingresaste es: \n\n");
-	for(i=1;i<=c;i=i+1)
-	{
-		for(j=1;j<=f;j=j+1)
-		{
+	for(i=1;i<=c;i=i+1)	{
+		for(j=1;j<=f;j=j+1)	{
 			printf("\t%i",A[i][j]);
 		}
 		printf("\n");
