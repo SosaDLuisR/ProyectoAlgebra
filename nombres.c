6867a64a4bedb4444b22387e9e6e3c4a3c7a0a26
Dayesc <104659710+Dayesc@users.noreply.github.com>
2022-05-04 16:00:39 -0500

    Create nombres.c

---
+++ nombres.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string curso = "Algebra lineal";
+  string profesor = "Gerardo Gonzalez Hernandez";
+  string nombre1 = "-Luis Ruben Sosa Delgadillo";
+  string nombre2 = "-Ezequiel Olivera Reyes";
+  string nombre3 = "-Jorge Ángel Jiménez Sandoval";
+  string nombre4 = "-Josué Hernández Rosas";
+  string nombre5 = "-Giovanna Daiana Escutia Silva";
+  
+  cout << "Materia: " << curso << "\n";
+  cout << "Profesor: " << profesor << "\n";
+  cout << "Integrantes: \n" << nombre1 << "\n" << nombre2 << "\n" << nombre3 << "\n" << nombre4 << "\n" << nombre5;
+}
